5a2ebd2b4f83a037d4f72f705812d3dfc8c58c30
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-08-04 19:38:54 -0300

    refactor: desaclopamento dos options na classe main

--- src/main/java/io/github/mgluiz/EasyClave.java
+++ src/main/java/io/github/mgluiz/EasyClave.java
@@ -1,37 +1,15 @@
 package io.github.mgluiz;
 
-import io.github.mgluiz.algorithms.Algorithms;
-import io.github.mgluiz.options.CryptOptions;
-import io.github.mgluiz.options.InputOptions;
+import io.github.mgluiz.options.ArgsOptions;
 import picocli.CommandLine;
-import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 
 @Command(
         name = "easyclave",
         mixinStandardHelpOptions = true,
         description = "EasyClave é uma ferramente de criptografia e descriptografia com o objetivo de ser facil, rápida e limpa",
         version = "EasyClave version 1.0 - 08/2025 \ndev: Luiz Brito (https://github.com/mgluizbrito)")
-public class EasyClave implements Runnable {
-
-    @ArgGroup(exclusive = true)
-    private CryptOptions crypt;
-
-    @ArgGroup(exclusive = false)
-    private InputOptions input;
-
-    @Option(names = {"-a", "--algo"}, arity = "1", paramLabel = "ALGORITHM", description = """
-            Algorítimo a ser utilizado para criptografia ou descriptografia.
-            
-            Algorítmos Simétricos (Opcional: inserir númer de bits da chave com o argumento -kb ou --keybits): AES, DES
-            Algorítmos Asimétricos (Utiliza-se de chaves públicas): RSA, ECC
-            Algorítmos de HASH (Não possuem chave, nem decrypt): SHA256, MD5
-            """)
-    private Algorithms algorithms;
-
-    @Option(names = {"-k", "--keyBits"}, arity = "1", description = "Número de bits da chave a ser utilizada para criptografia/descriptografia simétrica", paramLabel = "128")
-    private int keyBits = 128;
+public class EasyClave extends ArgsOptions implements Runnable {
 
     @Override
     public void run() {
@@ -52,7 +30,7 @@ public class EasyClave implements Runnable {
     }
 
     public static void main(String[] args){
-        int exitCode = new CommandLine(new EasyClave()).execute(args);
+        int exitCode = new CommandLine(new EasyClave()).execute();
         System.exit(exitCode);
     }
 }
---
+++ src/main/java/io/github/mgluiz/options/ArgsOptions.java
@@ -0,0 +1,25 @@
+package io.github.mgluiz.options;
+
+import io.github.mgluiz.algorithms.Algorithms;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Option;
+
+public abstract class ArgsOptions {
+    @ArgGroup(exclusive = true)
+    public CryptOptions crypt;
+
+    @ArgGroup(exclusive = false)
+    public InputOptions input;
+
+    @Option(names = {"-a", "--algo"}, arity = "1", paramLabel = "ALGORITHM", description = """
+            Algorítimo a ser utilizado para criptografia ou descriptografia.
+            
+            Algorítmos Simétricos (Opcional: inserir númer de bits da chave com o argumento -kb ou --keybits): AES, DES
+            Algorítmos Asimétricos (Utiliza-se de chaves públicas): RSA, ECC
+            Algorítmos de HASH (Não possuem chave, nem decrypt): SHA256, MD5
+            """)
+    public Algorithms algorithms;
+
+    @Option(names = {"-k", "--keyBits"}, arity = "1", description = "Número de bits da chave a ser utilizada para criptografia/descriptografia simétrica", paramLabel = "128")
+    public int keyBits = 128;
+}
