4d4db75d213a8e3bd64ff11acbf81dea02e5d0b1
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-08-04 16:10:54 -0300

    feat: enumeração de algorítimos suportados

---
+++ src/main/java/io/github/mgluiz/algorithms/Algorithms.java
@@ -0,0 +1,15 @@
+package io.github.mgluiz.algorithms;
+
+public enum Algorithms {
+    // Symmetric (Private Key)
+    AES,
+    DES,
+
+    // Asymmetric (Public Key)
+    RSA,
+    ECC,
+
+    // HASH (No Key)
+    SHA256,
+    MD5,
+}
