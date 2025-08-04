fadd50cee70a595aa45e6e5087cf7d4d3d739ed8
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-08-04 16:17:23 -0300

    feat: ALGO option adicionado

--- src/main/java/io/github/mgluiz/EasyClave.java
+++ src/main/java/io/github/mgluiz/EasyClave.java
@@ -1,31 +1,37 @@
 package io.github.mgluiz;
 
+import io.github.mgluiz.algorithms.Algorithms;
 import io.github.mgluiz.options.CryptOptions;
 import io.github.mgluiz.options.InputOptions;
 import picocli.CommandLine;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
-import java.io.File;
-import java.util.List;
 
 @Command(
         name = "easyclave",
         mixinStandardHelpOptions = true,
-        description = "EasyClave é uma ferramente de criptografia e descriptografia com o objetivo de ser facil, rapida e limpa",
+        description = "EasyClave é uma ferramente de criptografia e descriptografia com o objetivo de ser facil, rápida e limpa",
         version = "EasyClave version 1.0 - 08/2025 \ndev: Luiz Brito (https://github.com/mgluizbrito)")
 public class EasyClave implements Runnable {
 
     @ArgGroup(exclusive = true)
     private CryptOptions crypt;
 
-    @ArgGroup(exclusive = false, multiplicity = "1..2")
+    @ArgGroup(exclusive = false)
     private InputOptions input;
 
-    @Option(names = {"-k", "--key"}, arity = "1", description = "Chave a ser utilizada para criptografia/descriptografia simétrica")
-    private String key;
+    @Option(names = {"-a", "--algo"}, arity = "1", paramLabel = "ALGORITHM", description = """
+            Algorítimo a ser utilizado para criptografia ou descriptografia.
+            
+            Algorítmos Simétricos (Opcional: inserir númer de bits da chave com o argumento -kb ou --keybits): AES, DES
+            Algorítmos Asimétricos (Utiliza-se de chaves públicas): RSA, ECC
+            Algorítmos de HASH (Não possuem chave, nem decrypt): SHA256, MD5
+            """)
+    private Algorithms algorithms;
+
+    @Option(names = {"-k", "--keyBits"}, arity = "1", description = "Número de bits da chave a ser utilizada para criptografia/descriptografia simétrica", paramLabel = "128")
+    private int keyBits = 128;
 
     @Override
     public void run() {
@@ -46,7 +52,7 @@ public class EasyClave implements Runnable {
     }
 
     public static void main(String[] args){
-        int exitCode = new CommandLine(new EasyClave()).execute("-t", "pom.xml");
+        int exitCode = new CommandLine(new EasyClave()).execute(args);
         System.exit(exitCode);
     }
 }
