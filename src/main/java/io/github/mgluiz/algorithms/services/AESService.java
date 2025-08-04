d04c3e18fc94cccc045e1c31451f158daf786aa4
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-08-04 16:14:08 -0300

    feat: AES file encrypt adicionado

---
+++ src/main/java/io/github/mgluiz/algorithms/services/AESService.java
@@ -0,0 +1,49 @@
+package io.github.mgluiz.algorithms.services;
+
+import io.github.mgluiz.algorithms.Algorithms;
+import io.github.mgluiz.algorithms.interfaces.SymmetricAlgorithm;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.io.File;
+
+import static io.github.mgluiz.algorithms.interfaces.SymmetricAlgorithm.processFile;
+
+public class AESService implements SymmetricAlgorithm {
+    
+    @Override
+    public String msgEncrypt(String msg) {
+        return "";
+    }
+
+    @Override
+    public String msgEncrypt(String msg, int keyBites) {
+        return "";
+    }
+
+    @Override
+    public String msgDecrypt(String msg, SecretKey key) {
+        return "";
+    }
+
+    @Override
+    public void fileEncrypt(File inputFile, File outputFile) {
+        try {
+            SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES);
+            processFile(Cipher.ENCRYPT_MODE, inputFile, outputFile, key, "AES/CBC/PKCS5Padding");
+        } catch (Exception e) {}
+    }
+
+    @Override
+    public void fileEncrypt(File inputFile, File outputFile, int keyBits) {
+        try {
+            SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES, keyBits);
+            processFile(Cipher.ENCRYPT_MODE, inputFile, outputFile, key, "AES/CBC/PKCS5Padding");
+        } catch (Exception e) {}
+    }
+
+    @Override
+    public void fileDecrypt(SecretKey key, File file) {
+
+    }
+}
