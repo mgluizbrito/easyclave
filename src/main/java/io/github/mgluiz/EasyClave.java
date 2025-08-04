2fdbb7e6d77129e96bd78b53d0f456aeb0e7a43d
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-08-03 21:29:09 -0300

    feat: adicionado Options de Encrypt ou Decrypt

--- src/main/java/io/github/mgluiz/EasyClave.java
+++ src/main/java/io/github/mgluiz/EasyClave.java
@@ -1,23 +1,45 @@
 package io.github.mgluiz;
 
+import io.github.mgluiz.options.CryptOptions;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.ArgGroup;
 
 import java.util.concurrent.Callable;
 
 @Command(
         name = "easyclave",
         mixinStandardHelpOptions = true,
-        version = "checksum 4.0")
+        description = "EasyClave é uma ferramente de criptografia e descriptografia com o objetivo de ser facil, rapida e limpa",
+        version = "EasyClave version 1.0 - 08/2025 \ndev: Luiz Brito (https://github.com/mgluizbrito)")
 public class EasyClave implements Runnable {
+
+    @ArgGroup(exclusive = true)
+    CryptOptions crypt;
 
     @Override
     public void run() {
-        CommandLine.usage(this, System.out);
+
+        if (crypt == null){
+            CommandLine.usage(this, System.out);
+            return;
+        }
+
+        if (crypt.isEncrypt()){
+            System.out.println("Cripto");
+            return;
+        }
+
+        if (crypt.isDecrypt()){
+            System.out.println("Decripto");
+            return;
+        };
+
+
     }
 
     public static void main(String[] args){
-        int exitCode = new CommandLine(new EasyClave()).execute(args);
+        int exitCode = new CommandLine(new EasyClave()).execute();
         System.exit(exitCode);
     }
 }
---
+++ src/main/java/io/github/mgluiz/options/CryptOptions.java
@@ -0,0 +1,20 @@
+package io.github.mgluiz.options;
+
+import picocli.CommandLine.Option;
+
+public class CryptOptions {
+
+    @Option(names = {"-e", "--encrypt"}, description = "Ecrypt files or passwords with a key/algorithm of your choice")
+    public boolean encrypt;
+
+    @Option(names = {"-d", "--decrypt"}, description = "Decrypt files or passwords with a key/algorithm of your choice")
+    public boolean decrypt;
+
+    public boolean isEncrypt() {
+        return encrypt;
+    }
+
+    public boolean isDecrypt() {
+        return decrypt;
+    }
+}
