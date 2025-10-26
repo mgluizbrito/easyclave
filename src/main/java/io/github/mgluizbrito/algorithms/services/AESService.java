47d7e635228095c39839bd7e007c7b349433a2a7
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-10-26 01:21:37 -0300

    feat: AES cipher implemented

--- src/main/java/io/github/mgluizbrito/algorithms/services/AESService.java
+++ src/main/java/io/github/mgluizbrito/algorithms/services/AESService.java
@@ -6,50 +6,82 @@ import io.github.mgluizbrito.algorithms.interfaces.SymmetricAlgorithm;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import java.io.File;
-
-import static io.github.mgluizbrito.algorithms.interfaces.SymmetricAlgorithm.processFile;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.Base64;
 
 public class AESService implements SymmetricAlgorithm {
-    
+
+    private static final String ALGORITHM_MODE = "AES/ECB/PKCS5Padding";
+    private static final int DEFAULT_KEY_BITS = 128;
+
     @Override
-    public String msgEncrypt(String msg) {
-        return "";
+    public String encryptMessage(String plainText) throws GeneralSecurityException {
+        return this.encryptMessage(plainText, DEFAULT_KEY_BITS);
     }
 
     @Override
-    public String msgEncrypt(String msg, int keyBites) {
-        return "";
+    public String encryptMessage(String plainText, Integer keyBites) throws GeneralSecurityException {
+        SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES, keyBites);
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM_MODE);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        byte[] cipherTextBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(cipherTextBytes);
     }
 
     @Override
-    public String msgDecrypt(String msg, SecretKey key) {
-        return "";
+    public String decryptMessage(String cipherTextBase64, String keyBase64) throws GeneralSecurityException{
+        byte[] cipherTextBytes = Base64.getDecoder().decode(cipherTextBase64);
+        SecretKey key = SymmetricAlgorithm.base64KeyDecode(keyBase64, Algorithms.AES);
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM_MODE);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        byte[] plainTextBytes = cipher.doFinal(cipherTextBytes);
+
+        return new String(plainTextBytes, StandardCharsets.UTF_8);
     }
 
     @Override
-    public void fileEncrypt(File inputFile, File outputFile) {
-        try {
-
-            SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES);
-            processFile(Cipher.ENCRYPT_MODE, inputFile, outputFile, key, "AES/CBC/PKCS5Padding");
-        } catch (Exception e) {}
+    public void encryptFile(File inputFile, File outputFile) throws GeneralSecurityException, IOException {
+        this.encryptFile(inputFile, outputFile, DEFAULT_KEY_BITS);
     }
 
     @Override
-    public void fileEncrypt(File inputFile, File outputFile, int keyBits) {
-        try {
-
-            SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES, keyBits);
-            processFile(Cipher.ENCRYPT_MODE, inputFile, outputFile, key, "AES/CBC/PKCS5Padding");
-        } catch (Exception e) {}
+    public void encryptFile(File inputFile, File outputFile, int keyBits) throws GeneralSecurityException, IOException {
+        SecretKey key = SymmetricAlgorithm.generateKey(Algorithms.AES, keyBits);
+        processFile(Cipher.ENCRYPT_MODE, key, inputFile, outputFile);
     }
 
     @Override
-    public void fileDecrypt(String base64Key, File inputFile, File outputFile) {
-        try {
+    public void decryptFile(String base64Key, File inputFile, File outputFile) throws GeneralSecurityException, IOException {
+        SecretKey key = SymmetricAlgorithm.base64KeyDecode(base64Key, Algorithms.AES);
+        processFile(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
+    }
 
-            SecretKey key = SymmetricAlgorithm.base64KeyDecode(base64Key, Algorithms.AES);
-            processFile(Cipher.DECRYPT_MODE, inputFile, outputFile, key, "AES/CBC/PKCS5Padding");
-        }catch (Exception e){}
+    private static void processFile(int mode, SecretKey key, File inputFile, File outputFile) throws GeneralSecurityException, IOException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM_MODE);
+        cipher.init(mode, key);
+
+        try (FileInputStream inputStream = new FileInputStream(inputFile);
+             FileOutputStream outputStream = new FileOutputStream(outputFile))
+        {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+
+                byte[] output = cipher.update(buffer, 0, bytesRead);
+                if (output != null) outputStream.write(output);
+            }
+
+            byte[] output = cipher.doFinal();
+            if (output != null) outputStream.write(output);
+        }
     }
 }
